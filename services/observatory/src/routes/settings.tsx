9295cdb0204033b00559f13088531a6fed92364a
Gareth Price <garethprice@me.com>
2025-12-17 01:02:40 +0000

    chore(beads): close phlo-b1w

--- services/observatory/src/routes/settings.tsx
+++ services/observatory/src/routes/settings.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useEffect, useMemo, useState } from 'react'
 
+import type { ObservatorySettings } from '@/lib/observatorySettings'
 import {
   clearCacheEndpoint,
   getCacheStatsEndpoint,
